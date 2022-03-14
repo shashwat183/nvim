5edbed00461b090ff3cda0cdec5f0ab9bca457f5
shashwat pragya <shashwat183@gmail.com>
2022-03-14 03:17:08 +0000

    Set vscode theme as colorscheme

--- lua/shashwatpragya/colorscheme.lua
+++ lua/shashwatpragya/colorscheme.lua
@@ -4,4 +4,6 @@ vim.o.background = "dark" -- or "light" for light mode
 --     style = 'warm'
 -- }
 -- require('onedark').load()
-vim.cmd([[colorscheme gruvbox]])
+vim.g.vscode_style = "dark"
+vim.g.vscode_italic_comment = 1
+vim.cmd([[colorscheme vscode]])
