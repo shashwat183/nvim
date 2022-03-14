cf47e78e7cb3c36af71654d3cbcf7fce640e9223
shashwat pragya <shashwat183@gmail.com>
2022-03-14 03:16:06 +0000

    Add slant style, tab indicator and diagnostics to buffeline

--- lua/shashwatpragya/bufferline.lua
+++ lua/shashwatpragya/bufferline.lua
@@ -1,6 +1,17 @@
 vim.opt.termguicolors = true
-require('bufferline').setup {
+require("bufferline").setup({
 	options = {
-		offsets = {{filetype = "NvimTree", text = "File Explorer"}}
-	}
-}
+		offsets = { { filetype = "NvimTree", text = "File Explorer" } },
+		separator_style = "slant",
+		show_tab_indicators = true,
+		diagnostics = "nvim_lsp",
+		diagnostics_indicator = function(_, _, diagnostics_dict, _)
+			local s = " "
+			for e, n in pairs(diagnostics_dict) do
+				local sym = e == "error" and " " or (e == "warning" and " " or " ")
+				s = s .. n .. sym
+			end
+			return s
+		end,
+	},
+})
