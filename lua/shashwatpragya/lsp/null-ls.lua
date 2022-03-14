7af10b42857ec189613a3163e373fa085388ac31
shashwat pragya <shashwat183@gmail.com>
2022-03-14 03:11:48 +0000

    Add null-jls configuration
    Add flake8 and black configuration for python
    to null-ls. Add format on save functionality to
    null-ls. Add stylua for formatting lua files.

--- lua/shashwatpragya/lsp/null-ls.lua
+++ lua/shashwatpragya/lsp/null-ls.lua
@@ -4,12 +4,25 @@ local formatting = null_ls.builtins.formatting
 
 local diagnostics = null_ls.builtins.diagnostics
 
+local sources = {
+	formatting.stylua,
+	formatting.black.with({
+		extra_args = { "--line-length", "79", "--fast" },
+	}),
+	diagnostics.flake8,
+}
+
 null_ls.setup({
-    sources = {
-        formatting.stylua,
-        formatting.black.with({
-          extra_args = { "--line-length", "79", "--fast" }
-        }),
-        diagnostics.flake8,
-    },
+	sources = sources,
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd([[
+	            augroup LspFormatting
+	                autocmd! * <buffer>
+	                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+	            augroup END
+	            ]])
+		end
+	end,
 })
