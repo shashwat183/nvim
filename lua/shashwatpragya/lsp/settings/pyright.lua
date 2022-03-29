524d917ffda5009e3c91eb34f18e7a08c62c7242
shashwat pragya <shashwat183@gmail.com>
2022-03-29 19:37:15 +0530

    Add root dir and auto import to pyright

--- lua/shashwatpragya/lsp/settings/pyright.lua
+++ lua/shashwatpragya/lsp/settings/pyright.lua
@@ -1,8 +1,17 @@
+local util = require("lspconfig/util")
+
 return {
+	root_dir = function(fname)
+		return util.root_pattern(".git", "setup.py", "setup.cfg", "pyproject.toml", "requirements.txt")(fname)
+			or util.path.dirname(fname)
+	end,
 	settings = {
 		python = {
 			analysis = {
+				autoImportCompletions = true,
 				typeCheckingMode = "off",
+				autoSearchPaths = true,
+				diagnosticMode = "workspace",
 				-- diagnosticSeverityOverrides = {
 				-- 	reportGeneralTypeIssues = false,
 				-- },
