6dbc2436e5eb25401199634867e0f71f2e6528ce
shashwat pragya <shashwat183@gmail.com>
2022-07-29 14:37:30 +0100

    Add comment specifying how to use Json Schemas for yamlls
    Add comment specifying how to use Json Schemas for yamlls

--- lua/shashwatpragya/lsp/settings/yamlls.lua
+++ lua/shashwatpragya/lsp/settings/yamlls.lua
@@ -1,6 +1,10 @@
 return {
 	settings = {
 		yaml = {
+			-- No need to specify yaml configs here, doesn't seem to work, just use
+			-- inline metadata as specified in this blogpost from redhat -
+			-- https://developers.redhat.com/blog/2020/11/25/how-to-configure-yaml-schema-to-make-editing-files-easier#schema_association_without_a_file_name_pattern
+			-- Example :- # yaml-language-server: $schema=https://my.url.to/the/schema
 			-- schemas = {
 			-- 	["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = {
 			-- 		"/*.k8s.yaml",
