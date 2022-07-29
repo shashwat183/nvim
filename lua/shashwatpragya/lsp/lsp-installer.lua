99c81d52554761114eeae4572a460cdc93bb852a
shashwat pragya <shashwat183@gmail.com>
2022-07-29 14:04:37 +0100

    Add yamlls config
    Add empty yamlls config for future use.

--- lua/shashwatpragya/lsp/lsp-installer.lua
+++ lua/shashwatpragya/lsp/lsp-installer.lua
@@ -36,6 +36,11 @@ lsp_installer.on_server_ready(function(server)
 		opts = vim.tbl_deep_extend("force", gopls_opts, opts)
 	end
 
+	if server.name == "yamlls" then
+		local yamlls_opts = require("shashwatpragya.lsp.settings.yamlls")
+		opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
+	end
+
 	-- This setup() function will take the provided server configuration and decorate it with the necessary properties
 	-- before passing it onwards to lspconfig.
 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
---
+++ lua/shashwatpragya/lsp/settings/yamlls.lua
@@ -0,0 +1,15 @@
+return {
+	settings = {
+		yaml = {
+			-- schemas = {
+			-- 	["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = {
+			-- 		"/*.k8s.yaml",
+			-- 		"/k8s/*.k8s.yaml",
+			-- 	},
+			-- 	schemaStore = {
+			-- 		enable = true,
+			-- 	},
+			-- },
+		},
+	},
+}
