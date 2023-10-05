d9e203bc1036e9100bcb69b5c1cd62e949b9becf
shashwat pragya <shashwat183@gmail.com>
2023-10-05 23:13:08 +0100

    Add go specific indentation settings

---
+++ ftplugin/go.lua
@@ -0,0 +1,4 @@
+vim.opt["smartindent"] = true -- smart autoindenting when starting a new line
+vim.opt["expandtab"] = false -- convert tabs to spaces
+vim.opt["shiftwidth"] = 4 -- number of spaces for indentation
+vim.opt["tabstop"] = 4 -- number of spaces a tab appears as in buffer
