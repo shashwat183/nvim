b22248de8268408d6d2c7a61375001354a089993
shashwat pragya <shashwat183@gmail.com>
2023-10-05 20:29:57 +0100

    More exclude files in telescope

--- lua/shashwatpragya/telescope.lua
+++ lua/shashwatpragya/telescope.lua
@@ -1,6 +1,6 @@
 require('telescope').setup({
   defaults = {
-    file_ignore_patterns = { '^node_modules/', '.class', '^build/', '^bin/', '^.git/', '^vendor/', '^target/', '^venv/', '^sam/', '^.idea/', '%.settings', 'mvnw', 'mvnw.cmd', '.factorypath', '.project' },
+    file_ignore_patterns = { '^node_modules/', '.class', '^build/', '^bin/', '^.git/', '^vendor/', '^target/', '^venv/', '^sam/', '^.idea/', '%.settings', 'mvnw', 'mvnw.cmd', '.factorypath', '.project' , '.iml' },
     sorting_strategy = "ascending",
     prompt_prefix = " ",
     selection_caret = " ",
