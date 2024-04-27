0f0592890164d7320a23192fe05f72cd8af13970
shashwat pragya <shashwat183@gmail.com>
2024-04-27 23:08:42 +0100

    Some upgrades

--- lua/shashwatpragya/lsp/mason-lspconfig.lua
+++ lua/shashwatpragya/lsp/mason-lspconfig.lua
@@ -146,7 +146,6 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
 }
 
-
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 M.capabilities = require('cmp_nvim_lsp').default_capabilities(M.capabilities)
